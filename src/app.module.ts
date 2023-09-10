66731488be7f95732443c48f7e6a8b33027db1b2
Matias Troncoso <matias.troncoso.campos@gmail.com>
2023-09-10 10:08:55 -0300

    parks service v1 with city filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,12 @@ import { AppService } from './app.service';
 import { ParksModule } from './parks/parks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ParkEntity } from './parks/entity/park.entity';
-// import { parse } from 'pg-connection-string';
-
-// const databaseUrl = process.env.DATABASE_URL;
-// const connectionOptions = databaseUrl ? parse(databaseUrl) : null;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL, // Aquí va tu URL de conexión de ElephantSQL
+      url: process.env.DATABASE_URL,
       entities: [ParkEntity],
       synchronize: true,
       ssl:
@@ -27,25 +23,3 @@ import { ParkEntity } from './parks/entity/park.entity';
   providers: [AppService],
 })
 export class AppModule {}
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: connectionOptions ? connectionOptions.host : 'localhost',
-//       port: connectionOptions ? parseInt(connectionOptions.port, 10) : 5432,
-//       username: connectionOptions ? connectionOptions.user : 'ecoquerai',
-//       password: connectionOptions ? connectionOptions.password : '2357',
-//       database: connectionOptions
-//         ? connectionOptions.database
-//         : 'parks_service',
-//       entities: [ParkEntity],
-//       synchronize: true,
-//       ssl: connectionOptions ? { rejectUnauthorized: false } : false,
-//     }),
-//     ParksModule,
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
