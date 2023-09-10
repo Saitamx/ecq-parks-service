2d1487e4996d3583f2b3b34203b25d6707abb021
Matias Troncoso <matias.troncoso.campos@gmail.com>
2023-09-10 10:19:46 -0300

    Limpiar caché de Git

--- netlify/functions/server.ts
+++
@@ -1,9 +0,0 @@
-import { server } from '../../dist/src/main';
-
-export const handler = async (event: any, context: any) => {
-  const result = await server(event, context);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  };
-};
